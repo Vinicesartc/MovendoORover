3245c322d9125a36e7ebc70f982e385cac2ec58c
Vinicesartc <105077572+Vinicesartc@users.noreply.github.com>
2022-09-22 17:16:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+canvas=document.getElementById('mycanvas');
+ctx=canvas.getContext("2d");
+
+roverWidth=100;
+roverHeight=90;
+
+backgroundImage="mars.jpg";
+roverImage="rover.png";
+
+roverX=10;
+roverY=10;
+
+function add()
+{
+    backgroundImgTag=new Image();
+    backgroundImgTag.onload=uploadBackground;
+    backgroundImgTag.src=backgroundImage;
+
+    roverImgTag=new Image();
+    roverImgTag.onload=uploadrover;
+    roverImgTag.src=roverImage;
+
+
+}
+
+function uploadBackground()
+{
+    ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover()
+{
+    ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight);
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed=='38')
+    {
+        up();
+        console.log("seta para cima");
+    }
+
+    if(keyPressed=='40')
+    {
+        down();
+        console.log("seta para baixo");
+    }
+
+    if(keyPressed=='37')
+    {
+        left();
+        console.log("seta para a esquerda");
+    }
+
+    if(keyPressed=='39')
+    {
+        right();
+        console.log("seta para a direita");
+    }
+}
+
+function up()
+{
+    if(roverY>=0)
+    {
+        roverY = roverY-10;
+        console.log("Quando a seta direcional para cima for pressionada, X= "+roverX+" , Y= "+roverY);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function down()
+{
+    if(roverY<=500)
+    {
+        roverY = roverY+10;
+        console.log("Quando a seta direcional para baixo for pressionada, X= "+roverX+" , Y= "+roverY);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function left()
+{
+    if(roverX>=0)
+    {
+        roverX = roverX-10;
+        console.log("Quando a seta direcional para a esquerda for pressionada, X= "+roverX+" , Y= "+roverY);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function right()
+{
+    if(roverX<=700)
+    {
+        roverX = roverX+10;
+        console.log("Quando a seta direcional para a direita for pressionada, X= "+roverX+" , Y= "+roverY);
+        uploadBackground();
+        uploadrover();
+    }
+}
